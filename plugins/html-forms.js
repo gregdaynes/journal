d8c2158a35ad108c7d3ba122beaff9571ac93c2e
Gregory Daynes <email@gregdaynes.com>
2024-11-23 20:30:31 -0800

    chore: add multipart for data plugin

--- plugins/html-forms.js
+++ plugins/html-forms.js
@@ -1,6 +1,8 @@
 import fp from 'fastify-plugin'
 import formBody from '@fastify/formbody'
+import multipart from '@fastify/multipart'
 
 export default fp(async (fastify, opts) => {
   fastify.register(formBody, Object.assign(opts, {}))
+  fastify.register(multipart, Object.assign(opts, {}))
 })
